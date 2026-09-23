3149d0850680c1a31a3c144d49ce7594a31b090c
Jeff Ching <chingor@google.com>
2026-09-23 20:13:22 +0000

    chore: release circa 2026-09-23 (#1099)
    Bump library versions to 0.3.0 and regenerate client libraries for
    release.

--- Package.swift
+++ Package.swift
@@ -27,12 +27,12 @@ let package = Package(
     localOrRemotePackage(
       url: "https://github.com/googleapis/swift-google-rpc",
       path: "generated/swift-google-rpc",
-      from: "0.2.0"
+      from: "0.3.0"
     ),
     localOrRemotePackage(
       url: "https://github.com/googleapis/swift-google-wkt",
       path: "pkgs/swift-google-wkt",
-      from: "0.2.0"
+      from: "0.3.0"
     ),
   ],
   targets: [
